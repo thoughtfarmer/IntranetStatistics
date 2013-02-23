fdc4ac1497186c703dbb5a0f4cc4e08c26bd5e50
sgiehl <stefan@piwik.org>
2013-02-23 22:10:08 +0100

    updated widgetslist to use singleton instead of being complete static

--- core/PluginsFunctions/WidgetsList.php
+++ core/PluginsFunctions/WidgetsList.php
@@ -4,7 +4,7 @@
  * 
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
- * 
+ *
  * @category Piwik
  * @package PluginsFunctions
  */
@@ -14,163 +14,173 @@
  */
 class Piwik_WidgetsList
 {
-	/**
-	 * List of widgets
-	 *
-	 * @var array
-	 */
-	static protected $widgets = null;
+    /**
+     * @var Piwik_WidgetsList
+     */
+    static protected $_instance = null;
 
-	/**
-	 * Indicates whether the hook was posted or not
-	 *
-	 * @var bool
-	 */
-	static protected $hookCalled = false;
-	
-	/**
-	 * Returns all available widgets
-	 * The event WidgetsList.add is used to create the list
-	 *
-	 * @return array
-	 */
-	static public function get()
-	{
-		self::addWidgets();
-		Piwik_PostEvent('WidgetsList.get');
+    /**
+     * Contains all available widgets
+     * @var array
+     */
+    protected $widgets = array();
 
-		uksort(self::$widgets, array('Piwik_WidgetsList', '_sortWidgetCategories'));
-		
-		$widgets = array();
-		foreach(self::$widgets as $key => $v) 
-		{
-			if(isset($widgets[Piwik_Translate($key)])) {
-				$v = array_merge($widgets[Piwik_Translate($key)], $v);
-			}
-			$widgets[Piwik_Translate($key)] = $v;
-		}
-		return $widgets;
-	}
+    protected $eventPosted = false;
 
-	private static function addWidgets()
-	{
-		if (!self::$hookCalled) {
-			self::$hookCalled = true;
-			Piwik_PostEvent('WidgetsList.add');
-		}
-	}
+    /**
+     * non public constructor
+     */
+    protected function __construct()
+    {
+    }
 
-	/**
-	 * Sorting method for widget categories
-	 *
-	 * @param string  $a
-	 * @param string  $b
-	 * @return bool
-	 */
-	protected static function _sortWidgetCategories($a, $b)
-	{
-		$order = array(
-			'VisitsSummary_VisitsSummary',
-			'Live!',
-			'General_Visitors',
-			'UserSettings_VisitorSettings',
-			'Actions_Actions',
-			'Actions_SubmenuSitesearch',
-			'Referers_Referers',
-			'Goals_Goals',
-			'Goals_Ecommerce',
-			'_others_',
-			'Example Widgets',
-			'ExamplePlugin_exampleWidgets',
-		);
+    /**
+     * Returns the singleton instance
+     *
+     * @static
+     * @return Piwik_WidgetsList
+     */
+    static public function getInstance()
+    {
+        if (empty(self::$_instance)) {
+            self::$_instance = new self();
+        }
 
-		if(($oa = array_search($a, $order)) === false) {
-			$oa = array_search('_others_', $order);
-		}
-		if(($ob = array_search($b, $order)) === false) {
-			$ob = array_search('_others_', $order);
-		}
-		return $oa > $ob;
-	}
+        return self::$_instance;
+    }
 
-	/**
-	 * Adds an widget to the list
-	 *
-	 * @param string  $widgetCategory
-	 * @param string  $widgetName
-	 * @param string  $controllerName
-	 * @param string  $controllerAction
-	 * @param array   $customParameters
-	 */
-	static public function add($widgetCategory, $widgetName, $controllerName, $controllerAction, $customParameters)
-	{
-		$widgetName = Piwik_Translate($widgetName);
-		$widgetUniqueId = 'widget' . $controllerName . $controllerAction;
-		foreach($customParameters as $name => $value)
-		{
-			if (is_array($value))
-			{
-				// use 'Array' for backward compatibility;
-				// could we switch to using $value[0]?
-				$value = 'Array';
-			}
-			$widgetUniqueId .= $name . $value;
-		}
-		self::$widgets[$widgetCategory][] = array( 
-					'name' => $widgetName,
-					'uniqueId' => $widgetUniqueId,
-					'parameters' => array (	'module' => $controllerName,
-											'action' => $controllerAction
-										) + $customParameters
-									);
-	}
+    protected function _loadWidgetsList ()
+    {
+        if (!$this->eventPosted) {
+            Piwik_PostEvent('WidgetsList.add');
+            $this->eventPosted = true;
 
+            // append all available datatables as widgets
+            $dataTables = Piwik_DataTableList::getInstance()->get();
 
-	static public function remove($widgetCategory, $widgetName = false)
-	{
-		if(empty($widgetName)) {
-			unset(self::$widgets[$widgetCategory]);
-			return;
-		}
-		foreach(self::$widgets[$widgetCategory] as $id => $widget) {
-			if($widget['name'] == $widgetName) {
-				unset(self::$widgets[$widgetCategory][$id]);
-				return;
-			}
-		}
-	}
+            foreach ($dataTables AS $table) {
+                if ($table['isAvailableAsWidget']) {
+                    self::add($table['category'], $table['name'], $table['uniqueId'], $table['defaultParameters']);
+                }
+            }
+        }
 
-	/**
-	 * Checks if the widget with the given parameters exists in der widget list
-	 *
-	 * @param string  $controllerName
-	 * @param string  $controllerAction
-	 * @return bool
-	 */
-	static public function isDefined($controllerName, $controllerAction)
-	{
-		$widgetsList = self::get();
-		foreach($widgetsList as $widgetCategory => $widgets) 
-		{
-			foreach($widgets as $widget)
-			{
-    			if($widget['parameters']['module'] == $controllerName
-    				&& $widget['parameters']['action'] == $controllerAction)
-    			{
-    				return true;
-    			}
-			}
-		}
-		return false;
-	}
+        uksort($this->widgets, array($this, '_sortWidgetCategories'));
+
+        $widgets = array();
+        foreach ($this->widgets as $key => $v) {
+            if (isset($widgets[Piwik_Translate($key)])) {
+                $v = array_merge($widgets[Piwik_Translate($key)], $v);
+            }
+            $widgets[Piwik_Translate($key)] = $v;
+        }
+        $this->widgets = $widgets;
+    }
+
+    public function add($widgetCategory, $widgetName, $widgetUniqueId, $customParameters)
+    {
+        $widgetName = Piwik_Translate($widgetName);
+
+        if (empty($customParameters['module']) && empty($customParameters['action'])) {
+            $customParameters['module'] = 'CoreHome';
+            $customParameters['action'] = 'renderDataTableWidget';
+        }
+
+        $this->widgets[$widgetCategory][] = array(
+            'name'              => $widgetName,
+            'uniqueId'          => $widgetUniqueId,
+            'parameters'        => array(
+                                         'module' => $customParameters['module'],
+                                         'action' => $customParameters['action']
+                                   ),
+            'defaultParameters' => $customParameters
+        );
+    }
+
+    /**
+     * Return all available widgets grouped by category
+     *
+     * @return array
+     */
+    public function get()
+    {
+        $this->_loadWidgetsList();
+        return $this->widgets;
+    }
+
+    /**
+     * Returns the data of the widget with the given uniqueId
+     *
+     * @param string $uniqueId
+     *
+     * @return array
+     */
+    public function getWidgetByUniqueId($uniqueId)
+    {
+        foreach ($this->get() AS $widgets) {
+            foreach ($widgets AS $widget) {
+                if ($widget['uniqueId'] == $uniqueId) {
+                    return $widget;
+                }
+            }
+        }
+        return array();
+    }
+
+    /**
+     * Checks if a widget with the given uniqueId is defined
+     *
+     * @param $uniqueId
+     *
+     * @return bool
+     */
+    public function isDefined ($uniqueId)
+    {
+        $widget = $this->getWidgetByUniqueId($uniqueId);
+        return !empty($widget);
+    }
+
+    /**
+     * Sorting method for widget categories
+     *
+     * @param string  $a
+     * @param string  $b
+     *
+     * @return bool
+     */
+    protected function _sortWidgetCategories($a, $b)
+    {
+        $order = array(
+            'VisitsSummary_VisitsSummary',
+            'Live!',
+            'General_Visitors',
+            'UserSettings_VisitorSettings',
+            'Actions_Actions',
+            'Actions_SubmenuSitesearch',
+            'Referers_Referers',
+            'Goals_Goals',
+            'Goals_Ecommerce',
+            '_others_',
+            'Example Widgets',
+            'ExamplePlugin_exampleWidgets',
+        );
+
+        if (($oa = array_search($a, $order)) === false) {
+            $oa = array_search('_others_', $order);
+        }
+        if (($ob = array_search($b, $order)) === false) {
+            $ob = array_search('_others_', $order);
+        }
+        return $oa > $ob;
+    }
+
 
     /**
      * Method to reset the widget list
      * For testing only
      */
     public static function _reset() {
-        self::$widgets = null;
-        self::$hookCalled = false;
+        self::$_instance = new self();
     }
 }
 
@@ -180,10 +190,12 @@ class Piwik_WidgetsList
  * @see Piwik_WidgetsList::get
  *
  * @return array
+ *
+ * @deprecated since 1.10
  */
 function Piwik_GetWidgetsList()
 {
-	return Piwik_WidgetsList::get();
+	return Piwik_WidgetsList::getInstance()->get();
 }
 
 /**
@@ -196,10 +208,25 @@ function Piwik_GetWidgetsList()
  * @param string  $controllerName
  * @param string  $controllerAction
  * @param array   $customParameters
+ *
+ * @deprecated since 1.10
  */
 function Piwik_AddWidget( $widgetCategory, $widgetName, $controllerName, $controllerAction, $customParameters = array())
 {
-	Piwik_WidgetsList::add($widgetCategory, $widgetName, $controllerName, $controllerAction, $customParameters);
+    $widgetUniqueId = 'widget' . $controllerName . $controllerAction;
+    foreach ($customParameters as $name => $value) {
+        if (is_array($value)) {
+            // use 'Array' for backward compatibility;
+            // could we switch to using $value[0]?
+            $value = 'Array';
+        }
+        $widgetUniqueId .= $name . $value;
+    }
+
+    $customParameters['module'] = $controllerName;
+    $customParameters['action'] = $controllerAction;
+
+    Piwik_WidgetsList::getInstance()->add($widgetCategory, $widgetName, $widgetUniqueId, $customParameters);
 }
 
 /**
@@ -210,8 +237,11 @@ function Piwik_AddWidget( $widgetCategory, $widgetName, $controllerName, $contro
  * @param string  $controllerName
  * @param string  $controllerAction
  * @return bool
+ *
+ * @deprecated since 1.10
  */
 function Piwik_IsWidgetDefined($controllerName, $controllerAction)
 {
-	return Piwik_WidgetsList::isDefined($controllerName, $controllerAction);
+    $widgetUniqueId = 'widget' . $controllerName . $controllerAction;
+    return Piwik_WidgetsList::getInstance()->isDefined($widgetUniqueId);
 }
